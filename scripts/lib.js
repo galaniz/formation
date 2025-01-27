3c6561504e4262ca118d668caf3d029b84a39d42
Graciela Alaniz <graciela.alaniz@getmaple.ca>
2025-01-27 09:36:12 -0500

    update: pnpm lock

--- scripts/lib.js
+++ scripts/lib.js
@@ -10,12 +10,8 @@ import { cp, glob } from 'node:fs/promises'
 
 /* Recurse src for scss files and copy to lib */
 
-try {
-  for await (const entry of glob('src/**/*.scss')) {
-    await cp(entry, entry.replace('src', 'lib'), {
-      recursive: true
-    })
-  }
-} catch (error) {
-  console.error('[FRM] Error copying scss files ', error)
+for await (const entry of glob('src/**/*.scss')) {
+  await cp(entry, entry.replace('src', 'lib'), {
+    recursive: true
+  })
 }
